301e7364c863385803d32b65586400746ba354c2
YusufSyed-Singularis <yusuf.syed@singularisfuture.com>
2025-08-19 00:02:26 -0500

    exposing metrics to prometheus

--- backend/trace-prometheus.js
+++ backend/trace-prometheus.js
@@ -2,12 +2,15 @@ const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
 const { getNodeAutoInstrumentations}  = require('@opentelemetry/auto-instrumentations-node');
 const { PeriodicExportingMetricReader, ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
+const { PrometheusExporter } = require("@opentelemetry/exporter-prometheus");
 
+const prometheusExporter = new PrometheusExporter(
+    { port: 9464 },
+    () => {console.log("Prometheus metrics at: http://localhost:9464/metrics");}
+);
 const sdk = new NodeSDK({
   traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  }),
+  metricReader: prometheusExporter,
   instrumentations: [getNodeAutoInstrumentations()],
 });
 
